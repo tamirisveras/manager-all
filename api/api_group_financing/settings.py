a7b5be3f2cf4316307b2d84f3b64653d5dc5a062
tamirisveras <justamiris@gmail.com>
2024-09-04 13:11:39 -0300

    style

--- api/api_group_financing/settings.py
+++ api/api_group_financing/settings.py
@@ -137,13 +137,7 @@ CORS_ALLOWED_ORIGINS = [
 
 STATIC_URL = 'static/'
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_USE_TLS = True
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'wendersonoscardev@gmail.com'
-EMAIL_HOST_PASSWORD = 'qcul zhpc cuks zghc'
-DEFAULT_FROM_EMAIL= 'default from email'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
